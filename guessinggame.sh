72b211f7ea9698b8baca44e4cb6b2d03e059a29c
CoderRohan <55316158+CoderRohan@users.noreply.github.com>
2020-11-23 12:22:03 +0530

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,21 @@
+
+function guess(){
+    true_ans=$(ls -l |grep "^-"|wc -l)
+    while true;
+    do
+        echo "Please enter your guess.."
+        read  number
+        if [ $number -lt $true_ans ]
+        then
+            echo "Your guess is less then the true number"
+        elif [ $number -gt $true_ans ]
+        then
+            echo "Your guess is greater then the true number"
+        else
+            echo " Congratulation,you are right!"
+        break;
+        fi
+    done
+}
+echo "Guess the files number in the current directory!"
+guess
